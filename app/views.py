4d48e3c835a6822fa19ea00aa675fb3b1f69189a
Dayna Clare <daycanimate@gmail.com>
2024-02-21 16:44:24 -0600

    Completed exercise 1, steps 1-5

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -16,6 +16,23 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html', joined=format_date_joined(2016, 4, 21))
+
+
+###
+# Functions for application.
+###
+
+def format_date_joined(yr, mon, day):
+    date_joined = datetime.date(yr, mon, day)
+    ## Format the date to return only month and year date 
+    return ("Joined " + date_joined.strftime("%B, %Y"))
+
 
 
 ###
